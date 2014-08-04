07ee1dfe4d57e961e5212944602a29bf5b50db06
Vanessa Dean <vanessa.dean@gmail.com>
2014-08-04 17:21:22 -0400

    Devise working

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :photo, :email, :phone)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :households
 
   devise_for :users
+  resources :users, :only => [:index, :show] 
+
+  root 'pets#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
