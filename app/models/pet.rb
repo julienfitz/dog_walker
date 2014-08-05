3a9b9005d97d19be0251587fdc34016a454d7482
Vanessa Dean <vanessa.dean@gmail.com>
2014-08-05 16:37:50 -0400

    Fix sign out link and vet field in form

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,6 +5,6 @@ class Pet < ActiveRecord::Base
   belongs_to :vet
   has_one :supply
 
-  accepts_nested_attributes_for :vet
+  accepts_nested_attributes_for :vet, :reject_if => lambda { |c| c[:name].blank? }
 
 end
--- db/migrate/20140804193148_change_type_to_species.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTypeToSpecies < ActiveRecord::Migration
-  def change
-    rename_column :pets, :type, :species
-  end
-end
