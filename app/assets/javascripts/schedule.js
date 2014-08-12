259725902033e833ec667a8256675c5e84fba2f3
Vanessa Dean <vanessa.dean@gmail.com>
2014-08-12 09:15:47 -0400

    Adjust view

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -2,6 +2,10 @@ $(function(){
   $("#delete-appt").click(function(){
     var div_id = "#"+$(this).parents().data("id")
     console.log(div_id);
+    $("#create-appt").html("");
     $(div_id).remove();
   });
+
 });
+
+
