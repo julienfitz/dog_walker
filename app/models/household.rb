fbf50b23ef4bd9010a870424f5a8af024fe0fc48
Vanessa Dean <vanessa.dean@gmail.com>
2014-08-12 18:20:10 -0400

    Pets dependent destroy

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,7 +1,7 @@
 class Household < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   belongs_to :walker, class_name: "User"
-  has_many :pets
+  has_many :pets, dependent: :destroy
   validates_presence_of :email
 
   accepts_nested_attributes_for :owner
